6b5e0ed4cb60946d422ce8d4504abb54779a145f
shyye <emma.art.dev@gmail.com>
2024-12-03 08:16:44 +0100

    Add Day 2 part 2

--- 02/part2.py
+++ 02/part2.py
@@ -1,5 +1,5 @@
-# PATH_NAME = 'inputs/02_1-2.txt'
-PATH_NAME = 'inputs/test.txt'
+PATH_NAME = 'inputs/02_1-2.txt'
+# PATH_NAME = 'inputs/test.txt'
 
 MIN_DIFF = 1
 MAX_DIFF = 3
@@ -22,39 +22,30 @@ def isReportSafe(list):
         current = int(list[current_index])
         next = int(list[current_index + 1])
 
-        # Check diff
+        # Check if diff is not between MIN_DIFF and MAX_DIFF / not valid
         diff = abs(current - next)
         if (diff < MIN_DIFF or diff > MAX_DIFF):
-            
-            # If next index is the last index, the report can be safe if this error is removed.
-            isLastIndex = (current_index + 1) == last_index
-            if (isLastIndex):
-                print("hej")
-                error_count += 1
-            else:
-                # [current, next, nextNext] Check current and next-next value
-                current_index += 1
-                current = int(list[current_index])
-                next = int(list[current_index + 1])
-
-                nextDiff = abs(current - next)
-                if (nextDiff < MIN_DIFF or nextDiff > MAX_DIFF):
-                    return False
-                else:
-                    error_count += 1
+            error_count += 1
+            # Remove the next element after current
+            list.pop(current_index + 1)
+            # Move back the pointer to compare the current value with the value 
+            # that comes after the removed value e.g.:
+            #    [current, <removed value>, next]
+            current_index -= 1
+            last_index = len(list) - 1
 
         # Check decreasing requirement
         if (is_decreasing_list):
             if (next > current):
                 error_count += 1
 
-            # Check increasing requirement
+        # Check increasing requirement
         if (is_increasing_list):
             if (next < current):
                 error_count += 1
-            
+
         current_index += 1
-    
+
     if (error_count > ERROR_TOLERATION):
         return False
 
@@ -68,11 +59,10 @@ with open(PATH_NAME) as file:
         if (isReportSafe(levels)):
             numberOfSafeReports += 1
 
-        print(isReportSafe(levels))
-
 print(numberOfSafeReports)
 
 # One hour challenge:
 #   Attempt 1 fail
 #   Attempt 2 fail
-#       Time left: ??
+#   Attempt 3
+#       Time left: ?? isch 50 min
