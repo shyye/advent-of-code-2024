b98eeb5f8a163832f9f7bb4510db1ddb3d3a7f49
shyye <emma.art.dev@gmail.com>
2024-12-03 20:29:18 +0100

    Add Day 3 part 2

--- 03/part2.py
+++ 03/part2.py
@@ -1,15 +1,13 @@
 import re
 
-# PATH_NAME = 'inputs/03_1-2.txt'
-PATH_NAME = 'inputs/test.txt'
+PATH_NAME = 'inputs/03_1-2.txt'
+# PATH_NAME = 'inputs/test.txt'
 
-# mul -  Match "mul".
-# \(  -  Parentheses escaped with backslash.
-# \d+ -  Match one or more digits.
-# ,   -  Match comma
-DONT_PATTERN = r"don't\(\)"
-DO_PATTERN = r"do\(\)"
 MUL_PATTERN = r"mul\(\d+,\d+\)"
+PATTERN_BETWEEN_DONT_AND_DO = r"(?:don't\(\).*?do\(\))"
+
+# Everything between 'don't()' and 'do()' or 'don't()' and end of string
+PATTERN_BETWEEN_DONT_AND_DO_AND_END_OF_LINE = r"(?:don't\(\).*?do\(\)|don't\(\).*?$)"  
 
 REGEX_PATTERN_numbers = r"\d+"
 
@@ -17,17 +15,23 @@ REGEX_PATTERN_numbers = r"\d+"
 result = 0
 with open(PATH_NAME) as file:
 
+    # Concatenate to one string and remove newline character to avoid missing regex matches
+    data_string = ''
     for line in file:
-        matches = re.findall(DO_PATTERN, line)
+        data_string += line.strip()
 
-        print(matches)
+    # Remove every sequence that is betwwen don't() and do()
+    cleaned_string = re.sub(PATTERN_BETWEEN_DONT_AND_DO_AND_END_OF_LINE, "", data_string)
 
-        # for mul in matches:
-        #     numbers = re.findall(REGEX_PATTERN_numbers, mul)
-        #     result += int(numbers[0]) * int(numbers[1])
+    matches = re.findall(MUL_PATTERN, cleaned_string)
+    for mul in matches:
+        
+        numbers = re.findall(REGEX_PATTERN_numbers, mul)
+        result += int(numbers[0]) * int(numbers[1])
 
 print(result)
 
 # One hour challenge:
 #   Attempt 1 fail
-#       Time left: 0 min
+#   Attempt 2:
+#       Time left: 29 min
