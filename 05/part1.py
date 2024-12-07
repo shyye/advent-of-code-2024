7f710597983dc6afe41d334030c89df4f8351a4a
shyye <emma.art.dev@gmail.com>
2024-12-07 21:15:24 +0100

    Add Day 5 part 1

--- 05/part1.py
+++ 05/part1.py
@@ -1,5 +1,5 @@
-# PATH_NAME = 'inputs/05_1-2.txt'
-PATH_NAME = 'inputs/test.txt'
+PATH_NAME = 'inputs/05_1-2.txt'
+# PATH_NAME = 'inputs/test.txt'
 
 RULES = []
 PAGES = []
@@ -7,10 +7,47 @@ PAGES = []
 with open(PATH_NAME) as file:
 
     rules_and_pages = file.read().split('\n\n')
-    cleaned_pages = rules_and_pages[1].replace('\n', ',')
-    
-    RULES = rules_and_pages[0].split('\n')
-    PAGES = cleaned_pages.split(',')
 
-for p in pages:
+    rules = rules_and_pages[0].split('\n')
+    pages = rules_and_pages[1].split('\n')
     
+    # Save rules
+    for r in rules:
+        rule_items = r.split('|')
+        RULES.append(rule_items)
+
+    # Save pages
+    for p in pages:
+        page_items = p.split(',')
+        PAGES.append(page_items)
+        
+result = 0
+for page_update in PAGES:
+
+    isCorrectOrder = True
+
+    for rule in RULES:
+
+        is_subset = set(rule).issubset(set(page_update))
+
+        if is_subset:
+
+            # Index of element that should be before
+            b_index = page_update.index(rule[0])
+            # Index of element that should be after
+            a_index = page_update.index(rule[1])
+
+            # Switch elements if thet are in wrong order
+            if b_index > a_index:
+                isCorrectOrder = False
+                page_update[b_index], page_update[a_index] = page_update[a_index], page_update[b_index]
+        
+    # Calculate mid value of originally corrected ordered pages
+    if isCorrectOrder:
+        middle_index = len(page_update) // 2
+        middle_number = int(page_update[middle_index])
+
+        result += middle_number
+                
+
+print(result)
