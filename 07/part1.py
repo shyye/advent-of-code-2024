9052e59a0601d5dae84fec4ef780b64b0c0e3951
shyye <emma.art.dev@gmail.com>
2024-12-20 14:05:02 +0100

    Add Day 7 part 1

--- 07/part1.py
+++ 07/part1.py
@@ -1,9 +1,11 @@
 import itertools
 
-# PATH_NAME = 'inputs/07_1-2.txt'
-PATH_NAME = 'inputs/test.txt'
+PATH_NAME = 'inputs/07_1-2.txt'
+# PATH_NAME = 'inputs/test.txt'
 
 OPERATORS = ['*', '+']
+
+correct_values_sum = 0
 
 with open(PATH_NAME) as file:
 
@@ -12,13 +14,46 @@ with open(PATH_NAME) as file:
         test_value, numbers = line.strip().split(':')
 
         test_value = int(test_value)
-        # operators = operators.strip().split(' ')
         numbers = [int(num) for num in numbers.strip().split(' ')]
 
-        # TODO: combonatorics
+        # Combonatorics - Cartesian Product
+        # Find all combinations of operators, order matters
         num_of_operators = len(numbers) - 1
         operator_combinations = list(itertools.product(OPERATORS, repeat=num_of_operators))
 
-        # Calculate for each combination
+        # Check all possible combination
         for combination in operator_combinations:
+
+            # Calculate sum for this combination
+            # E.g. 1 + 2 * 3
+            sum = 0
+            for i in range(num_of_operators):
+
+                if i == 0:
+                    number_before = numbers[i]
+                else:
+                    # The sum of the previous calculations
+                    number_before = sum
+
+                operator = combination[i]
+                number_after = numbers[i + 1]
+
+                if operator == '*':
+                    sum = number_before * number_after
+                elif operator == '+':
+                    sum = number_before + number_after
+
+                # Break if the sum is invalid before the whole expression has been calculated 
+                if sum > test_value:
+                    break
             
+            if sum == test_value:
+                correct_values_sum += test_value
+                break
+
+print(correct_values_sum)
+
+# One hour challenge:
+#   Attempt 1 fail
+#   Attempt 2:
+#       Time left: 8 min
